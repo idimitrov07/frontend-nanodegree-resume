79192bd89afe071405574ed675eaa2cd1338d81d
Ivan <idimitrov07@gmail.com>
2014-11-24 22:31:17 +0200

    Edit resuma data.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,19 @@
-$("#main").append("Ivan");
+//
+// var awesomeThoughts = "I am Ivan and I am AWESOME!";
+// // console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//
+// $("#main").append(funThoughts);
+
+// var email = "ivan@udacity.com";
+// var newEmail = email.replace("udacity", "gmail");
+// console.log(email);
+// console.log(newEmail);
+var name = "Ivan";
+var formatedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Junior Web Developer";
+var formatedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formatedRole);
+$("#header").prepend(formatedName);
